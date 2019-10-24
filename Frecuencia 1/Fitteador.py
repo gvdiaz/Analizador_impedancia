f4da6753fe7a04a4e25d83efde696791d137189c
Gustavo V. Diaz <gus838@gmail.com>
2019-10-24 07:15:09 -0300

    primera versión de fitteador, quedó cualquiera

---
+++ Frecuencia 1/Fitteador.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 24 06:24:40 2019
+
+@author: tanita
+"""
+
+import numpy as np
+from scipy.optimize import *
+import matplotlib.pyplot as plt
+
+def seno(x,a,f,fase):
+    return (a*np.sin((2*np.pi*f*x)+fase))
+
+with np.load('Ensayo 1 CH1.npz') as archivo:
+    time_V1 = archivo['x']
+    V1 = archivo['y']
+    
+print (V1.shape)
+
+popt, pcov = curve_fit(seno, time_V1, V1 )
+
+print (popt)
+
+plt.plot( time_V1, V1,'b-', label = 'datos' )
+
+plt.plot(time_V1, seno(time_V1, *popt), 'r-',label='fit: a=%5.3f, b=%5.3f, c=%5.3f' % tuple(popt))
+
+
+
+plt.title( 'Time domain data. V1.' )
+plt.xlabel( 'Time (s)' )
+plt.ylabel( 'Volts' )
+plt.grid( True )
+plt.show()
